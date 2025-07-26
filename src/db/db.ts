8d15fa4078f9f928a9130f914d5233f85ad64e1a
ptzburn <milan.hokkanen@gmail.com>
2025-07-26 16:23:07 +0300

    neon instead of dockered db

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,12 +1,8 @@
 import "jsr:@std/dotenv/load";
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema.ts";
 
-export const pool = new Pool({
-  connectionString: Deno.env.get("DATABASE_URL")!,
-  max: 10,
-  idleTimeoutMillis: 30000,
-});
+export const sql = neon(Deno.env.get("DATABASE_URL")!);
 
-export const db = drizzle(pool, { schema, casing: "snake_case" });
+export const db = drizzle({ client: sql, schema, casing: "snake_case" });
